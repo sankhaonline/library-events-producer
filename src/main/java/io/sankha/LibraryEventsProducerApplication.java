e7cd60c7d9287220c0c321b4d411853543ea5807
Sankha Subhra Ghosh <sankhaonline@users.noreply.github.com>
2023-11-20 21:31:18 +0530

    Initial commit

---
+++ src/main/java/io/sankha/LibraryEventsProducerApplication.java
@@ -0,0 +1,13 @@
+package io.sankha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventsProducerApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(LibraryEventsProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/sankha/LibraryEventsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sankha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
